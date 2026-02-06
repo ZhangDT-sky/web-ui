4a767c81dc8535e4fa4a4153bf3a3381fe5b9f7c
ZhangDT-sky <485918776@qq.com>
2026-02-06 14:56:40 +0800

    手动添加strtobool

--- src/webui/components/browser_settings_tab.py
+++ src/webui/components/browser_settings_tab.py
@@ -1,5 +1,4 @@
 import os
-from distutils.util import strtobool
 import gradio as gr
 import logging
 from gradio.components import Component
@@ -8,6 +7,20 @@ from src.webui.webui_manager import WebuiManager
 from src.utils import config
 
 logger = logging.getLogger(__name__)
+
+def strtobool(val: str) -> bool:
+    """Convert a string representation of truth to True or False.
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        return False
 
 async def close_browser(webui_manager: WebuiManager):
     """
